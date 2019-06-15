5ff5003a4ca5c235f3ad6650a3a3e9062b9e3352
Daniel Vassallo <dvassallo83@gmail.com>
2019-06-15 00:13:03 -0700

    Parameterize the infra.yml script

--- deploy/deploy-infra.sh
+++ deploy/deploy-infra.sh
@@ -15,4 +15,8 @@ aws cloudformation deploy \
       GitHubRepo=encrypted.dev \
       GitHubBranch=master \
       GitHubPersonalAccessToken=$ACCESS_TOKEN \
+      EC2KeyPair=encrypted-staging \
+      EC2InstanceType=t3.micro \
+      EC2AMI=ami-0cb72367e98845d43 \
+      Domain=encrypted.dev \
     --capabilities CAPABILITY_NAMED_IAM
