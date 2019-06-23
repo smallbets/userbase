af2b7e2263afb91e3197279c277ff72d8e114462
Daniel Vassallo <dvassallo83@gmail.com>
2019-06-22 23:39:08 -0700

    Replace Let's Encrypt with AWS Cert Manager
    - Let's Encrypt has a limit of 5 certs per week for a single domain, and this was problematic when setting up and tearing down the stack.
    - AWS Certificate Manager requires a load balancer, so now the staging host sits behind an NLB.

--- deploy/deploy-infra.sh
+++ deploy/deploy-infra.sh
@@ -18,4 +18,5 @@ aws cloudformation deploy \
       EC2KeyPair=encd \
       EC2InstanceType=t3.micro \
       Domain=encrypted.dev \
+      Certificate=arn:aws:acm:us-west-2:446495294306:certificate/98cc709e-332e-4adf-ad3a-9e508c087e74 \
     --capabilities CAPABILITY_NAMED_IAM
