92e60cba114589b5e04ed002a77718a6820085f4
Daniel Vassallo <dvassallo83@gmail.com>
2019-07-01 07:24:09 -0700

    Enable TLS between the server and the load balancer
    - The load balancer terminates TLS with the domain's certificate, but transport between the server and the LB now also happens over TLS with a self-signed certificate.

--- deploy/deploy-infra.sh
+++ deploy/deploy-infra.sh
@@ -10,13 +10,12 @@ aws cloudformation deploy \
     --stack-name encd \
     --template-file ./deploy/infra.yml \
     --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-      GitHubOwner=dvassallo \
-      GitHubRepo=encrypted.dev \
+      GitHubOwner=encrypted-dev \
+      GitHubRepo=proof-of-concept \
       GitHubBranch=master \
       GitHubPersonalAccessToken=$ACCESS_TOKEN \
-      EC2KeyPair=encd \
       EC2InstanceType=t3.micro \
       Domain=encrypted.dev \
-      Certificate=arn:aws:acm:us-west-2:446495294306:certificate/98cc709e-332e-4adf-ad3a-9e508c087e74 \
-    --capabilities CAPABILITY_NAMED_IAM
+      Certificate=arn:aws:acm:us-west-2:446495294306:certificate/98cc709e-332e-4adf-ad3a-9e508c087e74
--- deploy/install-cert.sh
+++ deploy/install-cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /home/ec2-user/encd/keys
-cp /etc/letsencrypt/live/*/* /home/ec2-user/encd/keys/
+cp /home/ec2-user/keys/* /home/ec2-user/encd/keys/
 chown -R ec2-user:ec2-user /home/ec2-user/encd/keys
