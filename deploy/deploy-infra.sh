9a36bc167ccb9762ff73ae529a8af28ae05a5b60
Daniel Vassallo <dvassallo83@gmail.com>
2019-07-03 13:11:49 -0700

    Use different instance types for staging and demo

--- deploy/deploy-infra.sh
+++ deploy/deploy-infra.sh
@@ -48,7 +48,8 @@ aws cloudformation deploy \
     GitHubRepo=proof-of-concept \
     GitHubBranch=master \
     GitHubPersonalAccessToken=$ACCESS_TOKEN \
-    EC2InstanceType=t3.micro \
+    EC2StagingInstanceType=t3.micro \
+    EC2DemoInstanceType=c5.large \
     EC2AMI=ami-082b5a644766e0e6f \
     Domain=encrypted.dev \
     Certificate=arn:aws:acm:us-west-2:446495294306:certificate/98cc709e-332e-4adf-ad3a-9e508c087e74 \
