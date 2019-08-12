407546b854fbe0c8c15b8eb60b6ccb944e2f124c
Justin Berman <justinberman95@gmail.com>
2019-08-12 16:34:07 -0700

    Client can now read JSON error response from arraybuffer type endpoint

--- src/client/encrypted-dev-sdk/api/auth.js
+++ src/client/encrypted-dev-sdk/api/auth.js
@@ -1,23 +1,33 @@
 import axios from 'axios'
+import { readArrayBufferAsString } from '../utils'
 
 const TEN_SECONDS_MS = 10 * 1000
 const TIMEOUT = TEN_SECONDS_MS
 
 const signUp = async (username, password, userId, publicKey) => {
-  const signUpResponse = await axios({
-    method: 'POST',
-    url: '/api/auth/sign-up',
-    data: {
-      username,
-      password,
-      userId,
-      publicKey
-    },
-    responseType: 'arraybuffer',
-    timeout: TIMEOUT
-  })
-  const encryptedValidationMessage = signUpResponse.data
-  return encryptedValidationMessage
+  try {
+    const signUpResponse = await axios({
+      method: 'POST',
+      url: '/api/auth/sign-up',
+      data: {
+        username,
+        password,
+        userId,
+        publicKey
+      },
+      responseType: 'arraybuffer',
+      timeout: TIMEOUT
+    })
+    const encryptedValidationMessage = signUpResponse.data
+    return encryptedValidationMessage
+  } catch (e) {
+    if (e.response && e.response.data) {
+      // necessary conversion for arraybuffer response type
+      e.response.data = JSON.parse(await readArrayBufferAsString(e.response.data))
+    }
+
+    throw e
+  }
 }
 
 const validateKey = async (validationMessage) => {
--- src/client/encrypted-dev-sdk/utils.js
+++ src/client/encrypted-dev-sdk/utils.js
@@ -34,3 +34,13 @@ export const sizeOfDdbItems = (items) => {
 export const getSecondsSinceT0 = (t0) => {
   return `${((performance.now() - t0) / 1000).toFixed(2)}`
 }
+
+export const readArrayBufferAsString = (arrayBuffer) => {
+  return new Promise(resolve => {
+    let reader = new FileReader()
+    reader.onload = () => {
+      resolve(reader.result)
+    }
+    reader.readAsText(new Blob([arrayBuffer]))
+  })
+}
