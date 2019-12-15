852f0681ee325ed56db5ba456ca310c276fc7ebd
Tony D'Addeo <tony@askhr.com>
2019-12-14 20:33:05 -0600

    Use more advanced core-js features
    https://babeljs.io/docs/en/babel-plugin-transform-runtime#version

--- src/userbase-js/webpack.config.js
+++ src/userbase-js/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env) => {
 
     config = {
       entry: {
-        main: './src/userbase-js/'
+        main: './src/userbase-js/index.js'
       },
       output: {
         path: path.join(__dirname, '../../npm_build/userbase-js/'),
@@ -66,7 +66,7 @@ module.exports = (env) => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: [['@babel/plugin-transform-runtime', { corejs: 3, useESModules: true }]]
+              plugins: [['@babel/plugin-transform-runtime', { corejs: 3, useESModules: true, version: "^7.7.6" }]]
             }
           }
         }
