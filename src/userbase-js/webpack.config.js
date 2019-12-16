c2bef27daa4ae16b5073587cd886e6530151a811
Tony D'Addeo <tony@askhr.com>
2019-12-16 11:30:55 -0600

    Mark userbase-js as side effect free
    This will enable better tree shaking on the SDK when it is bundled by
    end developers.
    
    See https://developers.google.com/web/fundamentals/performance/optimizing-javascript/tree-shaking

--- src/userbase-js/webpack.config.js
+++ src/userbase-js/webpack.config.js
@@ -55,6 +55,7 @@ module.exports = (env) => {
         {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
+          sideEffects: true,
         },
         {
           enforce: 'pre',
