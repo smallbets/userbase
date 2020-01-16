7caa12eb83de471bc605b30de28071f98be833e4
Daniel Vassallo <dvassallo83@gmail.com>
2020-01-15 19:57:55 -0800

    Rename resource prefix from beta to prod-v1

--- src/userbase-server/setup.js
+++ src/userbase-server/setup.js
@@ -17,12 +17,12 @@ const defineUsername = () => {
       appUsername = os.userInfo().username
     }
   } else {
-    appUsername = 'beta'
+    appUsername = 'prod-v1'
   }
   return appUsername
 }
 
-logger.info("Running as USER: " + defineUsername());
+logger.info("Running as USER: " + defineUsername())
 
 // if running in dev mode, prefix the DynamoDB tables and S3 buckets with the username
 const resourceNamePrefix = 'userbase-' + defineUsername() + '-'
