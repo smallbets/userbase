c2a7657769516dd029c730dfe6458662126fb721
Justin Berman <justinberman95@gmail.com>
2020-01-18 01:04:36 -0800

    Update poc to reflect parameter changes to SDK

--- src/proof-of-concept/client/components/User/logic.js
+++ src/proof-of-concept/client/components/User/logic.js
@@ -8,7 +8,7 @@ const _errorHandler = (e, operation) => {
 
 const signUp = async (username, password, email, rememberMe) => {
   try {
-    const user = await userbase.signUp(username, password, email, null, null, rememberMe)
+    const user = await userbase.signUp({ username, password, email, rememberMe })
     return user
   } catch (e) {
     return _errorHandler(e, 'sign up')
@@ -25,7 +25,7 @@ const signOut = async () => {
 
 const signIn = async (username, password, rememberMe) => {
   try {
-    const result = await userbase.signIn(username, password, rememberMe)
+    const result = await userbase.signIn({ username, password, rememberMe })
     return result
   } catch (e) {
     return _errorHandler(e, 'sign in')
