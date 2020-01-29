b116b94215dca068d7fad1e240cc2ab4c858d6e7
Justin Berman <justinberman95@gmail.com>
2020-01-29 02:14:20 -0800

    try catch all localData functions in case user has them turned off

--- src/userbase-js/src/localData.js
+++ src/userbase-js/src/localData.js
@@ -1,69 +1,89 @@
 const _getSeedName = (appId, username) => `userbaseSeed.${appId}.${username}`
 
 const setCurrentSession = (rememberMe, username, signedIn, sessionId, creationDate) => {
-  const session = { username, signedIn, sessionId, creationDate }
-  const sessionString = JSON.stringify(session)
+  try {
+    const session = { username, signedIn, sessionId, creationDate }
+    const sessionString = JSON.stringify(session)
 
-  if (rememberMe === 'local') {
-    localStorage.setItem('userbaseCurrentSession', sessionString)
-  } else if (rememberMe === 'session') {
-    sessionStorage.setItem('userbaseCurrentSession', sessionString)
+    if (rememberMe === 'local') {
+      localStorage.setItem('userbaseCurrentSession', sessionString)
+    } else if (rememberMe === 'session') {
+      sessionStorage.setItem('userbaseCurrentSession', sessionString)
+    }
+  } catch (e) {
+    // ok
   }
 }
 
 const getCurrentSession = () => {
-  const sessionStorageCurrentSessionString = sessionStorage.getItem('userbaseCurrentSession')
+  try {
+    const sessionStorageCurrentSessionString = sessionStorage.getItem('userbaseCurrentSession')
 
-  if (sessionStorageCurrentSessionString) {
-    const currentSession = JSON.parse(sessionStorageCurrentSessionString)
+    if (sessionStorageCurrentSessionString) {
+      const currentSession = JSON.parse(sessionStorageCurrentSessionString)
 
-    if (!currentSession.signedIn) {
-      const localCurrentSessionString = localStorage.getItem('userbaseCurrentSession')
+      if (!currentSession.signedIn) {
+        const localCurrentSessionString = localStorage.getItem('userbaseCurrentSession')
 
-      if (localCurrentSessionString) {
-        const localCurrentSession = JSON.parse(localCurrentSessionString)
+        if (localCurrentSessionString) {
+          const localCurrentSession = JSON.parse(localCurrentSessionString)
 
-        // allows session from localStorage to override sessionStorage if signed in
-        // to localStorage session and not signed in to sessionStorage session
-        if (localCurrentSession.signedIn) {
-          return {
-            ...localCurrentSession,
-            rememberMe: 'local'
+          // allows session from localStorage to override sessionStorage if signed in
+          // to localStorage session and not signed in to sessionStorage session
+          if (localCurrentSession.signedIn) {
+            return {
+              ...localCurrentSession,
+              rememberMe: 'local'
+            }
           }
         }
       }
+
+      return {
+        ...JSON.parse(sessionStorageCurrentSessionString),
+        rememberMe: 'session'
+      }
     }
 
-    return {
-      ...JSON.parse(sessionStorageCurrentSessionString),
-      rememberMe: 'session'
+    const localSessionString = localStorage.getItem('userbaseCurrentSession')
+    return localSessionString && {
+      ...JSON.parse(localSessionString),
+      rememberMe: 'local'
     }
-  }
-
-  const localSessionString = localStorage.getItem('userbaseCurrentSession')
-  return localSessionString && {
-    ...JSON.parse(localSessionString),
-    rememberMe: 'local'
+  } catch (e) {
+    // ok
   }
 }
 
 const saveSeedString = (rememberMe, appId, username, seedString) => {
-  if (rememberMe === 'local') {
-    localStorage.setItem(_getSeedName(appId, username), seedString)
-  } else if (rememberMe === 'session') {
-    sessionStorage.setItem(_getSeedName(appId, username), seedString)
+  try {
+    if (rememberMe === 'local') {
+      localStorage.setItem(_getSeedName(appId, username), seedString)
+    } else if (rememberMe === 'session') {
+      sessionStorage.setItem(_getSeedName(appId, username), seedString)
+    }
+  } catch (e) {
+    // ok
   }
 }
 
 const removeSeedString = (appId, username) => {
-  const seedName = _getSeedName(appId, username)
-  sessionStorage.removeItem(seedName)
-  localStorage.removeItem(seedName)
+  try {
+    const seedName = _getSeedName(appId, username)
+    sessionStorage.removeItem(seedName)
+    localStorage.removeItem(seedName)
+  } catch (e) {
+    // ok
+  }
 }
 
 const getSeedString = (appId, username) => {
-  const seedName = _getSeedName(appId, username)
-  return sessionStorage.getItem(seedName) || localStorage.getItem(seedName)
+  try {
+    const seedName = _getSeedName(appId, username)
+    return sessionStorage.getItem(seedName) || localStorage.getItem(seedName)
+  } catch (e) {
+    // ok
+  }
 }
 
 const signInSession = (rememberMe, username, sessionId, creationDate) => {
@@ -77,8 +97,12 @@ const signOutSession = (rememberMe, username) => {
 }
 
 const removeCurrentSession = () => {
-  sessionStorage.removeItem('userbaseCurrentSession')
-  localStorage.removeItem('userbaseCurrentSession')
+  try {
+    sessionStorage.removeItem('userbaseCurrentSession')
+    localStorage.removeItem('userbaseCurrentSession')
+  } catch (e) {
+    // ok
+  }
 }
 
 export default {
