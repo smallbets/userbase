cfb4afacfe93a407ef13199beb2ddb5838779304
Justin Berman <justinberman95@gmail.com>
2020-03-14 23:24:58 -0700

    Fix pagination for some admin panel queries

--- src/userbase-server/admin.js
+++ src/userbase-server/admin.js
@@ -1075,7 +1075,7 @@ const getAccessTokens = async function (adminId) {
   while (accessTokenResponse.LastEvaluatedKey) {
     params.ExclusiveStartKey = accessTokenResponse.LastEvaluatedKey
     accessTokenResponse = await ddbClient.query(params).promise()
-    accessTokens.push(accessTokenResponse.Items)
+    accessTokens.push(...accessTokenResponse.Items)
   }
 
   return accessTokens.map(accessToken => ({
