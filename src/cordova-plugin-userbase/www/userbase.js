b29f70f264e3a7b083e07b8829775456fb3e37ef
Justin Berman <justinberman95@gmail.com>
2021-01-04 17:40:35 -0800

    Cordova plugin: Scrypt hex str to Uint8Array for userbase-js compat.

--- src/cordova-plugin-userbase/www/userbase.js
+++ src/cordova-plugin-userbase/www/userbase.js
@@ -1,10 +1,23 @@
 // loads userbase on window object
 window.cordova.require('cordova-plugin-userbase.userbase-js')
 
+var hexStringToUint8Array = (hexString) => {
+  if (hexString.length % 2 !== 0) throw new Error('Hex string must be even length')
+  const halfHexStringLen = hexString.length / 2
+  const array = []
+  for (let i = 0; i < halfHexStringLen; i++) {
+    const byteStartIndex = i * 2
+    const byte = hexString.substring(byteStartIndex, byteStartIndex + 2)
+    const byteAsInt = parseInt(byte, 16)
+    array.push(byteAsInt)
+  }
+  return new Uint8Array(array)
+}
+
 var scrypt = function (passwordArrayBuffer, saltArrayBuffer, N, r, p, dkLen) {
   return new Promise(function (resolve, reject) {
     function successCallback(passwordHash) {
-      resolve(passwordHash)
+      resolve(hexStringToUint8Array(passwordHash))
     }
 
     function errorCallback(e) {
